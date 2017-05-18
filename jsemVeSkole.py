ad5eea9e61f73a4d13c1a1a403ee2ca1b062cd13
Martin Urbanec <martin@urbanec.cz>
2017-05-18 09:03:37 +0200

    Init

---
+++ jsemVeSkole.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import requests
+import json
+import os
+
+url = 'https://api.ipify.org?format=json'
+jsonData = requests.get(url).content
+data = json.loads(jsonData)
+if data['ip'] == '195.113.165.66':
+	print "Stopping local ssh"
+	os.system('service ssh stop')
+	print "Starting ssh tunel for GitHub"
+	os.system('autossh -p 2222 -f -i /home/martin/.ssh/id_rsa_tunelar -NL 22:github.com:22 tunelar@vps.urbanec.cz')
+else:
+	print "Doing nothing"
